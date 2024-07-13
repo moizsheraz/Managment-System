ac29152078468e37de7eca4cae6c805d3ec93c4e
Zain <learnerstudent001@gmail.com>
2024-07-13 15:49:25 +0500

    Header || Responsive

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -4,11 +4,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../features/auth/authSlice';
 import api from '../../api';
 import logo from '../../assets/logo.png'; // Update the path to your logo image
-import { FaHome, FaUser, FaSignInAlt, FaSignOutAlt, FaUserPlus, FaPen } from 'react-icons/fa';
+import { FaHome, FaUser, FaSignInAlt, FaSignOutAlt, FaUserPlus, FaPen, FaBars, FaTimes } from 'react-icons/fa';
 
 export default function Header() {
   const { isAuthenticated } = useSelector((state) => state.auth);
   const [users, setUsers] = useState(null);
+  const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation();
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -79,18 +80,27 @@ export default function Header() {
     navigate('/');
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="bg-gray-800 text-white p-4 shadow-md">
-      <nav className="container mx-auto flex justify-between items-center">
+      <nav className="container mx-auto flex justify-between items-center relative">
         <div className="flex items-center">
           <img src={logo} alt="BlissBuzz Logo" className="h-8 w-8 mr-2" />
           <div className="text-2xl font-bold">
             <Link to="/">BlissBuzz</Link>
           </div>
         </div>
-        <ul className="flex space-x-10">
+        <div className="md:hidden">
+          <button onClick={toggleMenu}>
+            {menuOpen ? <FaTimes className="text-2xl" /> : <FaBars className="text-2xl" />}
+          </button>
+        </div>
+        <ul className={`md:flex md:space-x-10 absolute md:static top-12 -right-4 bg-gray-800 md:bg-transparent z-10 ${menuOpen ? 'block' : 'hidden'} md:flex-row flex-col space-y-4 md:space-y-0 p-4 md:p-0 w-auto`}>
           {navItems.filter(item => item.show).map((item, index) => (
-            <li key={index} className={`${location.pathname === item.route ? 'border-b-2 border-white' : ''}`}>
+            <li key={index} className={`flex justify-center ${location.pathname === item.route ? 'border-b-2 border-white' : ''}`}>
               {item.name === "Logout" ? (
                 <button onClick={handleLogout} className="flex items-center hover:text-gray-400 transition duration-200">
                   {item.icon}
