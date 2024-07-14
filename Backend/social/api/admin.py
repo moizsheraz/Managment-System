379327b46ae6edb7ede6e26de2ca02571c9a8d94
Zain <learnerstudent001@gmail.com>
2024-07-14 13:09:56 +0500

    Change in Token Auth

--- Backend/social/api/admin.py
+++ Backend/social/api/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import User, Post, Tag, Follow
 # Register your models here.
-from rest_framework.authtoken.admin import TokenAdmin
-class CustomTokenAdmin(TokenAdmin):
-    search_fields = ['key', 'user__username']
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Tag)
